bcc75efb3989bc5a0f84f27de8d6fd65e1867a22
Mahesh Kansaar <camsacar@gmail.com>
2024-12-25 20:10:10 -0500

    updated apps

--- app.py
+++ app.py
@@ -16,10 +16,10 @@ from check_stationarity import check_stationarity
 from transformation import apply_transformations
 from arima import arima_model
 from random_forest import random_forest_model
-from lstm import lstm_model
+#from lstm import lstm_model
 from feedback import feedback_form
-from sarima_model import sarima_model
-from spectral_analysis import spectral_analysis 
+#from sarima_model import sarima_model
+#from spectral_analysis import spectral_analysis 
 
 
 # Initialize session state variables
@@ -86,15 +86,15 @@ def main():
         st.sidebar.button("Price Returns", on_click=set_page, args=("Price Returns",), key="price_returns_button")
         st.sidebar.button("Slope of Trend Lines", on_click=set_page, args=("Trend Slope",), key="trend_slope_button")
         st.sidebar.button("Volatility Indicators", on_click=set_page, args=("Volatility Indicators",), key="volatility_button")
-        st.sidebar.button("Spectral Analysis", on_click=set_page, args=("Spectral Analysis",), key="spectral_analysis_button")
+        #st.sidebar.button("Spectral Analysis", on_click=set_page, args=("Spectral Analysis",), key="spectral_analysis_button")
         st.sidebar.button("Time Series Decomposition", on_click=set_page, args=("Decomposition",), key="decomposition_button")
         st.sidebar.button("Preprocess Data", on_click=set_page, args=("Preprocess Data",), key="preprocess_data_button")
         st.sidebar.button("Check Stationarity", on_click=set_page, args=("Check Stationarity",), key="check_stationarity_button")
         st.sidebar.button("Transform Data", on_click=set_page, args=("Transform Data",), key="transform_data_button")
         st.sidebar.button("ARIMA Model", on_click=set_page, args=("ARIMA Model",), key="arima_button")
         st.sidebar.button("Random Forest Model", on_click=set_page, args=("Random Forest",), key="random_forest_button")
-        st.sidebar.button("SARIMA Model", on_click=set_page, args=("SARIMA Model",), key="sarima_button")
-        st.sidebar.button("LSTM Model", on_click=set_page, args=("LSTM",), key="lstm_button")
+        #st.sidebar.button("SARIMA Model", on_click=set_page, args=("SARIMA Model",), key="sarima_button")
+        #st.sidebar.button("LSTM Model", on_click=set_page, args=("LSTM",), key="lstm_button")
         st.sidebar.button("Contact Us", on_click=set_page, args=("Contact Us",), key="contact_us_button")
         st.sidebar.button("Feedback", on_click=set_page, args=("Feedback",), key="feedback_button")
         st.sidebar.button("Logout", on_click=logout, key="logout_button")
@@ -126,12 +126,12 @@ def main():
             arima_model()
         elif st.session_state.page == "Random Forest":
             random_forest_model()
-        elif st.session_state.page == "LSTM":
-            lstm_model()
-        elif st.session_state.page == "SARIMA Model":
-            sarima_model()
-        elif st.session_state.page == "Spectral Analysis":
-            spectral_analysis()
+        #elif st.session_state.page == "LSTM":
+        #    lstm_model()
+        #elif st.session_state.page == "SARIMA Model":
+        #    sarima_model()
+        #elif st.session_state.page == "Spectral Analysis":
+        #    spectral_analysis()
         elif st.session_state.page == "Contact Us":
             contact_us()
         elif st.session_state.page == "Feedback":
