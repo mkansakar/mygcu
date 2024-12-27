5012c5e227cf09f19d14de4e16c7a709ec4edb32
Mahesh Kansakar <camsacar@gmail.com>
2024-12-27 11:57:59 -0500

    bug fixes

--- data_visualization.py
+++ data_visualization.py
@@ -11,9 +11,10 @@ def visualize_data():
         return
     
     data = st.session_state['data']
-
+    st.write(data.tail())
+    
     st.title("Candlestick Chart")
-    st.markdown(f"Stock: {st.session_state['symbol']}")
+    #st.markdown(f"Stock: {st.session_state['symbol']}")
     # Ensure the required columns are present for the candlestick chart
     if all(col in data.columns for col in ['Open', 'High', 'Low', 'Close']):
         fig = go.Figure(
